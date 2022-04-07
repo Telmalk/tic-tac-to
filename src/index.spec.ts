555111ac0bf902374d2ceabeb8ace291f6675419
MosieurClement <clement.haller@noir.dev>
2022-04-07 16:28:06 +0200

    inital

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,67 @@
-it("That's a test!", function () {
-  expect(1 + 1).toEqual(2);
-});
+import { Board, Game, generateEmptyBoard, play, Position } from "./index";
+
+test("A player can put pawn on cell", function() {
+  const board: Board =  [
+    {x: 0, y: 0, content: ""},
+    {x: 0, y: 1, content: ""},
+    {x: 0, y: 2, content: ""},
+    {x: 1, y: 0, content: "X"},
+    {x: 1, y: 1, content: ""},
+    {x: 1, y: 2, content: ""},
+    {x: 2, y: 0, content: ""},
+    {x: 2, y: 1, content: ""},
+    {x: 2, y: 2, content: ""},
+  ]
+  const expectedBoard: Board = [
+    {x: 0, y: 0, content: "O"},
+    {x: 0, y: 1, content: ""},
+    {x: 0, y: 2, content: ""},
+    {x: 1, y: 0, content: "X"},
+    {x: 1, y: 1, content: ""},
+    {x: 1, y: 2, content: ""},
+    {x: 2, y: 0, content: ""},
+    {x: 2, y: 1, content: ""},
+    {x: 2, y: 2, content: ""},
+  ]
+  const position: Position = {
+    x: 0,
+    y: 0
+  }
+  expect(play(board, position, "O")).toEqual(expectedBoard)
+})
+/*
+test("A player can't put pawn on first cell", function() {
+  const board: Board =  [
+    {x: 0, y: 0, content: "O"},
+    {x: 0, y: 1, content: ""},
+    {x: 0, y: 2, content: ""},
+    {x: 1, y: 0, content: ""},
+    {x: 1, y: 1, content: ""},
+    {x: 1, y: 2, content: ""},
+    {x: 2, y: 0, content: ""},
+    {x: 2, y: 1, content: ""},
+    {x: 2, y: 2, content: ""},
+  ]
+  expect(play(board)).toThrow("Can't put pawn here")
+})
+*/
+/*
+test("is game finished", function() {
+  const expectedBoard: Board = [
+    {x: 0, y: 0, content: "X"},
+    {x: 0, y: 1, content: "O"},
+    {x: 0, y: 2, content: "X"},
+    {x: 1, y: 0, content: "O"},
+    {x: 1, y: 1, content: "X"},
+    {x: 1, y: 2, content: "O"},
+    {x: 2, y: 0, content: "X"},
+    {x: 2, y: 1, content: "O"},
+    {x: 2, y: 2, content: "X"},
+  ]
+
+  const expectGame: Game = {
+    status: "Finish",
+    board: expectedBoard,
+  }
+  expect(finishedGame(game)).toEqual()
+})*/
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+type Token = "" | "X" | "O"
+
+interface Cell {
+  x: 0 | 1 | 2
+  y: 0 | 1 | 2
+  content: Token
+}
+
+export type Board = Cell[]
+
+export type Game = {
+  status: string
+  board: Board
+}
+
+export type Position = {
+  x: 0 | 1 | 2
+  y: 0 | 1 | 2
+}
+
+export function generateEmptyBoard(): Board {
+  return [
+    {x: 0, y: 0, content: ""},
+    {x: 0, y: 1, content: ""},
+    {x: 0, y: 2, content: ""},
+    {x: 1, y: 0, content: ""},
+    {x: 1, y: 1, content: ""},
+    {x: 1, y: 2, content: ""},
+    {x: 2, y: 0, content: ""},
+    {x: 2, y: 1, content: ""},
+    {x: 2, y: 2, content: ""},
+  ]
+}
+
+/*
+Allo ? Allo?
+ */
+/*
+export function play(board: Board, {x, y}: Position, playerToken: Token ): Board {
+  const newBoard = board.map((cell) => {
+    if (cell.x === x && cell.y === y) {
+      cell.content = playerToken
+    }
+    return cell
+  })
+  console.log(newBoard)
+  return newBoard
+}
